390f816e9a50b5f879c1440ae6e7cd52472a8dd8
tysonzero <danielspencersmith@gmail.com>
2017-09-16 03:35:02 -0400

    Implement most of JavaScript.Web.Location

--- src/JavaScript/Web/Location.hs
+++ src/JavaScript/Web/Location.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module JavaScript.Web.Location ( Location
                                , getWindowLocation
                                , getHref
@@ -26,79 +28,89 @@ module JavaScript.Web.Location ( Location
                                , replace
                                ) where
 
-data Location = Location
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.IORef (IORef, modifyIORef, newIORef, readIORef)
+import Data.JSString hiding (replace)
+import System.IO.Unsafe (unsafePerformIO)
+
+newtype Location = Location (IORef (Map JSString JSString))
+
+windowLocation :: Location
+windowLocation = Location . unsafePerformIO $ newIORef mempty
+{-# NOINLINE windowLocation #-}
 
 getWindowLocation :: IO Location
-getWindowLocation = undefined
+getWindowLocation = pure windowLocation
 
 getHref :: Location -> IO JSString
-getHref = undefined
+getHref (Location l) = (M.! "href") <$> readIORef l
 
 setHref :: JSString -> Location -> IO ()
-setHref = undefined
+setHref v (Location l) = modifyIORef l $ M.insert "href" v
 
 getProtocol :: Location -> IO JSString
-getProtocol = undefined
+getProtocol (Location l) = (M.! "protocol") <$> readIORef l
 
 setProtocol :: JSString -> Location -> IO ()
-setProtocol = undefined
+setProtocol v (Location l) = modifyIORef l $ M.insert "protocol" v
 
 getHost :: Location -> IO JSString
-getHost = undefined
+getHost (Location l) = (M.! "host") <$> readIORef l
 
 setHost :: JSString -> Location -> IO ()
-setHost = undefined
+setHost v (Location l) = modifyIORef l $ M.insert "host" v
 
 getHostname :: Location -> IO JSString
-getHostname = undefined
+getHostname (Location l) = (M.! "hostname") <$> readIORef l
 
 setHostname :: JSString -> Location -> IO ()
-setHostname = undefined
+setHostname v (Location l) = modifyIORef l $ M.insert "hostname" v
 
 getPort :: Location -> IO JSString
-getPort = undefined
+getPort (Location l) = (M.! "port") <$> readIORef l
 
 setPort :: JSString -> Location -> IO ()
-setPort = undefined
+setPort v (Location l) = modifyIORef l $ M.insert "port" v
 
 getPathname :: Location -> IO JSString
-getPathname = undefined
+getPathname (Location l) = (M.! "pathname") <$> readIORef l
 
 setPathname :: JSString -> Location -> IO ()
-setPathname = undefined
+setPathname v (Location l) = modifyIORef l $ M.insert "pathname" v
 
 getSearch :: Location -> IO JSString
-getSearch = undefined
+getSearch (Location l) = (M.! "search") <$> readIORef l
 
 setSearch :: JSString -> Location -> IO ()
-setSearch = undefined
+setSearch v (Location l) = modifyIORef l $ M.insert "search" v
 
 getHash :: Location -> IO JSString
-getHash = undefined
+getHash (Location l) = (M.! "hash") <$> readIORef l
 
 setHash :: JSString -> Location -> IO ()
-setHash = undefined
+setHash v (Location l) = modifyIORef l $ M.insert "hash" v
 
 getUsername :: Location -> IO JSString
-getUsername = undefined
+getUsername (Location l) = (M.! "username") <$> readIORef l
 
 setUsername :: JSString -> Location -> IO ()
-setUsername = undefined
+setUsername v (Location l) = modifyIORef l $ M.insert "username" v
 
 getPassword :: Location -> IO JSString
-getPassword = undefined
+getPassword (Location l) = (M.! "password") <$> readIORef l
 
 setPassword :: JSString -> Location -> IO ()
-setPassword = undefined
+setPassword v (Location l) = modifyIORef l $ M.insert "password" v
 
 getOrigin :: Location -> IO JSString
-getOrigin = undefined
+getOrigin (Location l) = (M.! "origin") <$> readIORef l
 
 assign :: JSString -> Location -> IO ()
-assign = undefined
+assign _ _ = error "JavaScript.Web.Location.assign"
 
 reload :: Bool -> Location -> IO ()
-reload = undefined
+reload _ _ = error "JavaScript.Web.Location.reload"
 
 replace :: JSString -> Location -> IO ()
-replace = undefined
+replace _ _ = error "JavaScript.Web.Location.replace"
